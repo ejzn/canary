a0acd562b4ccaaacd27900200ee83d3089da6d90
Stephen Richter <stephenrichter15@gmail.com>
2017-11-13 22:31:02 -0800

    Added MemberValidationStarter to test AS400 Simulator.

--- src/main/java/com/consol/citrus/simulator/starter/DefaultTestStarter.java
+++ src/main/java/com/consol/citrus/simulator/starter/DefaultTestStarter.java
@@ -1,33 +1,32 @@
 package com.consol.citrus.simulator.starter;
 
-import com.consol.citrus.http.client.HttpClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.consol.citrus.simulator.model.ScenarioParameter;
 import com.consol.citrus.simulator.model.ScenarioParameterBuilder;
+import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.simulator.scenario.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Starter("DefaultTestStarter")
 public class DefaultTestStarter extends AbstractScenarioStarter {
 
-    // @Autowired
-    // private HttpClient hybrisClient;
+    @Autowired
+    private HttpClient hybrisClient;
 
     @Override
     public void run(ScenarioDesigner scenario) {
 
-      scenario.echo("test action executed!");
+      scenario.echo("Excuting a basic GET request");
         
       scenario.http()
-        .client("http://localhost:8080")
+        .client(hybrisClient)
         .send()
         .get("/services/rest");
 
       scenario.http()
-        .client("http://localhost:8080")
+        .client(hybrisClient)
         .receive()
         .response(HttpStatus.OK);
     }
--- src/main/java/com/consol/citrus/simulator/starter/MemberValidationStarter.java
+++ src/main/java/com/consol/citrus/simulator/starter/MemberValidationStarter.java
@@ -1,25 +1,25 @@
 package com.consol.citrus.simulator.starter;
 
-import com.consol.citrus.http.client.HttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.consol.citrus.simulator.model.ScenarioParameter;
 import com.consol.citrus.simulator.model.ScenarioParameterBuilder;
+import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.simulator.scenario.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Starter("MemberValidationStarter")
 public class MemberValidationStarter extends AbstractScenarioStarter {
 
-    // @Autowired
-    // private HttpClient hybrisClient;
+    @Autowired
+    private HttpClient hybrisClient;
 
     @Override
     public void run(ScenarioDesigner scenario) {
         
         scenario.http()
-                .client("http://localhost:8080")
+                .client(hybrisClient)
                 .send()
                 .post("/services/rest/membership/validation")
                 .contentType("application/xml")
